8e4aed8c7975d6ff5e7f208cb73121b38064a066
Ryan Hirsch <ryan.hirsch@mixpanel.com>
2021-03-24 10:00:13 -0500

    seperate resource checking from certificaiton

--- components/PhaseCertification/index.tsx
+++ components/PhaseCertification/index.tsx
@@ -18,6 +18,43 @@ interface PhaseCertificationProps {
 	cors: Record<string, boolean>;
 }
 
+const No: React.FunctionComponent<{ className?: string }> = (props) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			className={props.className ?? ""}
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth={2}
+				d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+			/>
+		</svg>
+	);
+};
+const Yes: React.FunctionComponent<{ className?: string }> = (props) => {
+	return (
+		<svg
+			className={props.className ?? ""}
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+				d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+			></path>
+		</svg>
+	);
+};
+
 export const PhaseCertification: React.FunctionComponent<PhaseCertificationProps> = (props) => {
 	const phases: PhaseDef[] = [
 		{
@@ -59,83 +96,129 @@ export const PhaseCertification: React.FunctionComponent<PhaseCertificationProps
 	};
 
 	return (
-		<div className="my-8">
-			<div className="flex justify-center">
-				<div className="mx-12 flex flex-col text-center border-4 border-purple-900 rounded w-72">
+		<>
+			<section className="my-8">
+				<div className="flex justify-center">
+					{phases.map((phase) => (
+						<div
+							className="mx-12 flex flex-col text-center border-4 border-purple-900 rounded w-56"
+							key={phase.name}
+						>
+							<header className="bg-purple-900 text-purple-50 py-1 pb-2 flex align-middle justify-center font-semibold">
+								<Link href={phase.url}>{phase.name}</Link>
+								{phase.status === PhaseStatus.Open ? <div className="text-xs mx-1">*</div> : null}
+							</header>
+							<ul className="px-3 py-4 mx-2">
+								{phase.tags.map((tag) => {
+									const iconSize = "w-6 h-6 mr-2";
+									const color = hasPhaseTag(phase, tag) ? "text-purple-900" : "text-purple-400";
+									return (
+										<li key={tag} className="flex my-1">
+											{hasPhaseTag(phase, tag) ? (
+												<Yes className={`${iconSize} ${color}`} />
+											) : (
+												<div className={iconSize} />
+											)}
+											<div className={`${color}`}>{tag}</div>
+										</li>
+									);
+								})}
+							</ul>
+						</div>
+					))}
+				</div>
+				<footer className="text-center mt-3 text-purple-900">
+					<div>
+						See{" "}
+						<Link
+							href="https://github.com/Podcastindex-org/podcast-namespace#element-list"
+							className="underline"
+						>
+							podcast-namespace
+						</Link>{" "}
+						for more information. Phase 3 implementation is still pending.
+					</div>
+				</footer>
+			</section>
+			<section className="my-12 flex flex-col items-center">
+				<header className="text-center my-4 max-w-5xl text-purple-900">
+					The resource checks below help determine how compatible a podcast feed is with the rest of
+					the web-based podcasting ecosystem. They do not impact the certification of a podcast,
+					merely a way to help understand how to be most compatible with all community projects.
+				</header>
+				<div className="flex justify-center">
+					<ResourceChecks checks={props.cors} />
+				</div>
+
+				<footer className="text-center mt-3 text-purple-900">
+					<div>
+						There is a known issue with some hosts (such as libsyn) for enclosure CORS detection due
+						to redirects.
+					</div>
+				</footer>
+			</section>
+		</>
+	);
+};
+
+const ResourceChecks: React.FunctionComponent<{ checks: Record<string, boolean> }> = (props) => {
+	type SupportedType = { name: string; isSupported: boolean };
+	type SupportGroup = { title: string; description: string; items: SupportedType[] };
+
+	const transformed: SupportedType[] = Object.entries(props.checks).map(([name, support]) => ({
+		name,
+		isSupported: support,
+	}));
+
+	function getItems(namePrefix: string, items: SupportedType[]): SupportedType[] {
+		return items
+			.filter((x) => x.name.startsWith(namePrefix))
+			.map((x) => ({ name: x.name.replace(namePrefix, ""), isSupported: x.isSupported }));
+	}
+
+	const groups: SupportGroup[] = [
+		{
+			title: "HTTPS",
+			description: "",
+			items: getItems("https", transformed),
+		},
+		{
+			title: "CORS",
+			description: "",
+			items: getItems("cors", transformed),
+		},
+		{
+			title: "Hotlink",
+			description: "",
+			items: getItems("hotlink", transformed),
+		},
+	];
+
+	return (
+		<>
+			{groups.map(({ title, description, items }) => (
+				<div
+					key={title}
+					className="mx-12 flex flex-col text-center border-4 border-purple-900 rounded w-64"
+				>
 					<header className="bg-purple-900 text-purple-50 py-1 pb-2 flex align-middle justify-center font-semibold">
-						Resource Checks
+						{title}
 					</header>
 					<ul className="px-3 py-4 mx-2">
-						{Object.entries(props.cors).map(([name, support]) => {
-							const color = support ? "text-purple-900" : "text-purple-300";
+						{items.map(({ name, isSupported }) => {
+							const color = isSupported ? "text-purple-900" : "text-purple-400";
+							const Icon = isSupported ? Yes : No;
+
 							return (
 								<li key={name} className="flex my-1">
-									<svg
-										className={`w-6 h-6 mr-2 ${color}`}
-										fill="none"
-										stroke="currentColor"
-										viewBox="0 0 24 24"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="2"
-											d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-										></path>
-									</svg>
+									<Icon className={`w-6 h-6 mr-2 ${color}`}></Icon>
 									<div className={`${color}`}>{name}</div>
 								</li>
 							);
 						})}
 					</ul>
 				</div>
-				{phases.map((phase) => (
-					<div
-						className="mx-12 flex flex-col text-center border-4 border-purple-900 rounded w-72"
-						key={phase.name}
-					>
-						<header className="bg-purple-900 text-purple-50 py-1 pb-2 flex align-middle justify-center font-semibold">
-							<Link href={phase.url}>{phase.name}</Link>
-							{phase.status === PhaseStatus.Open ? <div className="text-xs mx-1">*</div> : null}
-						</header>
-						<ul className="px-3 py-4 mx-2">
-							{phase.tags.map((tag) => {
-								const color = hasPhaseTag(phase, tag) ? "text-purple-900" : "text-purple-300";
-								return (
-									<li key={tag} className="flex my-1">
-										<svg
-											className={`w-6 h-6 mr-2 ${color}`}
-											fill="none"
-											stroke="currentColor"
-											viewBox="0 0 24 24"
-											xmlns="http://www.w3.org/2000/svg"
-										>
-											<path
-												strokeLinecap="round"
-												strokeLinejoin="round"
-												strokeWidth="2"
-												d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
-											></path>
-										</svg>
-										<div className={`${color}`}>{tag}</div>
-									</li>
-								);
-							})}
-						</ul>
-					</div>
-				))}
-			</div>
-			<footer className="text-center mt-3 text-purple-700">
-				<div>
-					See{" "}
-					<Link href="https://github.com/Podcastindex-org/podcast-namespace#element-list">
-						podcast-namespace
-					</Link>{" "}
-					for more information. Phase 3 implementation is still pending. <br />
-					There is a known issue with libsyn hosted enclosure CORS detection due to redirects.
-				</div>
-			</footer>
-		</div>
+			))}
+		</>
 	);
 };
